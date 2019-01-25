7768053729855807bde26ba37cf6b634e21ba30e
ant_on_su <a.sugonyako@gmail.com>
2019-01-25 23:08:04 +0100

    excel->df

--- notebooks/try1.py
+++ notebooks/try1.py
@@ -5,17 +5,24 @@
 #%%
 import pandas as pd
 import xlrd
+import numpy as np
 
 #%%
-df = pd.read_excel("./data/Copy of wyniki baza danych 5 dec 2018.xls")
+df = pd.read_excel("./data/Copy of wyniki baza danych 5 dec 2018.xls", 
+header=0, convert_float=True)
 df.head()
 
 #%%
 df = df[df.komfort != 'komfort']
-df.dropna(axis=0, how="all", inplace=True)
-df.dropna(axis=1, how="all", inplace=True)
+df = df.applymap(lambda x: x.strip() if type(x) is str else x)
+df=df.apply(pd.to_numeric, errors='ignore')
+df.dropna(axis='columns', how="all", inplace=True)
+df.dropna(axis='rows', how="all", inplace=True)
 df.shape
 
 #%%
-df = df.apply(pd.to_numeric, errors='ignore', downcast='integer')
+df['M3']=df['M3'].map({'ukraina' : 1, 'polska' :0})
+df.head()
+
+#%%
 df.dtypes
